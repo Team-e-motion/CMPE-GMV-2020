b53457801be4f2bc3d1a3926867d799f5e79a007
Arturacu <arturoacuaviva@gmail.com>
2020-11-24 01:18:29 +0100

    Cleaning material ui + creating basic structure

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App extends Component {
                 <div className="App">
                     <NavBar />
                     <Switch>
+                        {/*My life is useless*/}
                         <Route exact path="/CMPE-GMV-2020" component={Home} />
                         <Route path="/CMPE-GMV-2020/certify" component={Certify} />
                         <Route path="/CMPE-GMV-2020/validate" component={Validate} />
---
+++ src/components/navrouter/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+import { Home } from '../home';
+import { Certify } from '../certify';
+import Validate from '../validate';
+import { About } from '../about';
+
+export const NavigationBar = () => {
+    return (
+        <BrowserRouter>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/CMPE-GMV-2020">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/CMPE-GMV-2020/certify">Certify</Link>
+                    </li>
+                    <li>
+                        <Link to="/CMPE-GMV-2020/validate">Validate</Link>
+                    </li>
+                    <li>
+                        <Link to="/CMPE-GMV-2020/about">About</Link>
+                    </li>
+                </ul>
+                <Route exact path="/CMPE-GMV-2020" component={Home} />
+                <Route exact path="/CMPE-GMV-2020/certify" component={Certify} />
+                <Route exact path="/CMPE-GMV-2020/validate" component={Validate} />
+                <Route exact path="/CMPE-GMV-2020/about" component={About} />
+            </nav>
+        </BrowserRouter>
+    );
+};
